f4c18e4deb9faa131b5211ccf1944fc0a38ee505
Nikhil Narayana <metonym@live.com>
2016-05-03 19:00:38 -0400

    Remove old TBA API and import new one. Integrate match data into description. Large number of if statements that need to be cut down somehow.

--- youtubeup.py
+++ youtubeup.py
@@ -12,14 +12,14 @@ import datetime
 from apiclient.errors import HttpError
 from apiclient.http import MediaFileUpload
 from oauth2client.tools import argparser
-from TheBlueAlliance import *
+from TBA import *
 from addtoplaylist import add_video_to_playlist
 from updateThumbnail import update_thumbnail
 from youtubeAuthenticate import get_authenticated_service
 
 NOW = datetime.datetime.now()
 
-#Default Variables - A lot needs to be changed based on event
+# Default Variables - A lot needs to be changed based on event
 DEFAULT_VIDEO_CATEGORY = 28
 DEFAULT_THUMBNAIL = "thumbnail.png"
 DEFAULT_PLAYLIST_ID = "PL9UFVOe2UANx7WGnZG57BogYFKThwhIa2"
@@ -30,15 +30,15 @@ EVENT_NAME = "Indiana State Championship"
 EVENT_CODE = "2016incmp"
 QUAL = "Qualification Match %s"
 QUARTER = "Quarterfinal Match %s"
-QUARTERT = "Quarterfinal Tiebreaker %s" #Dear FIRST, what the hell?
+QUARTERT = "Quarterfinal Tiebreaker %s"  # Dear FIRST, what the hell?
 SEMI = "Semifinal Match %s"
 SEMIT = "Semifinal Tiebreaker %s"
 FINALS = "Finals Match %s"
 FINALST = "Finals Tiebreaker"
 EXTENSION = ".mp4"
-DEFAULT_TITLE = YEAR + " " + ORGANIZATION + " " + EVENT_NAME + " - " + QUAL #CHANGE BASED ON EVENT
-DEFAULT_FILE = YEAR + " " + ORGANIZATION + " " + EVENT_NAME + " - " + QUAL + EXTENSION #CHANGE BASED ON EVENT
-MATCH_TYPE = ["qm%d","qf%dm%d","sf%dm%d","f1m%d"]
+DEFAULT_TITLE = YEAR + " " + ORGANIZATION + " " + EVENT_NAME + " - " + QUAL  # CHANGE BASED ON EVENT
+DEFAULT_FILE = YEAR + " " + ORGANIZATION + " " + EVENT_NAME + " - " + QUAL + EXTENSION  # CHANGE BASED ON EVENT
+MATCH_TYPE = ["qm", "qf", "sf", "f1m"]
 DEFAULT_DESCRIPTION = "Footage of the " + YEAR + " " + ORGANIZATION + " " + EVENT_NAME + " Event is courtesy the IndianaFIRST AV Crew." + """
 
 Blue Alliance (%d, %d, %d) - %d
@@ -54,103 +54,192 @@ Thanks for watching!"""
 
 VALID_PRIVACY_STATUSES = ("public", "private", "unlisted")
 
+
+def get_match_code(mcode, mnum, tbreak):
+    if mcode == "qm":
+        match_code = mcode + mnum
+        return EVENT_CODE, match_code
+    if mcode == "qf":
+        if mnum == 1:
+            match_code = mcode + "1" + "m" + "1"
+            return EVENT_CODE, match_code
+        if mnum == 2:
+            match_code = mcode + "2" + "m" + "1"
+            return EVENT_CODE, match_code
+        if mnum == 3:
+            match_code = mcode + "3" + "m" + "1"
+            return EVENT_CODE, match_code
+        if mnum == 4:
+            match_code = mcode + "4" + "m" + "1"
+            return EVENT_CODE, match_code
+        if mnum == 5:
+            match_code = mcode + "1" + "m" + "2"
+            return EVENT_CODE, match_code
+        if mnum == 6:
+            match_code = mcode + "2" + "m" + "2"
+            return EVENT_CODE, match_code
+        if mnum == 7:
+            match_code = mcode + "3" + "m" + "2"
+            return EVENT_CODE, match_code
+        if mnum == 8:
+            match_code = mcode + "4" + "m" + "2"
+            return EVENT_CODE, match_code
+        if mnum == 9:
+            match_code = mcode + "1" + "m" + "3"
+            return EVENT_CODE, match_code
+        if mnum == 10:
+            match_code = mcode + "2" + "m" + "3"
+            return EVENT_CODE, match_code
+        if mnum == 11:
+            match_code = mcode + "3" + "m" + "3"
+            return EVENT_CODE, match_code
+        if mnum == 12:
+            match_code = mcode + "4" + "m" + "3"
+            return EVENT_CODE, match_code
+    if mcode == "sf":
+        if mnum == 1:
+            match_code = mcode + "1" + "m" + "1"
+            return EVENT_CODE, match_code
+        if mnum == 2:
+            match_code = mcode + "2" + "m" + "1"
+            return EVENT_CODE, match_code
+        if mnum == 3:
+            match_code = mcode + "2" + "m" + "2"
+            return EVENT_CODE, match_code
+        if mnum == 4:
+            match_code = mcode + "2" + "m" + "2"
+            return EVENT_CODE, match_code
+        if mnum == 5:
+            match_code = mcode + "2" + "m" + "3"
+            return EVENT_CODE, match_code
+        if mnum == 6:
+            match_code = mcode + "2" + "m" + "3"
+            return EVENT_CODE, match_code
+    if mcode == "f1m":
+        if mnum == 1:
+            match_code = mcode + "1"
+            return EVENT_CODE, match_code
+        if mnum == 2:
+            match_code = mcode + "2"
+            return EVENT_CODE, match_code
+        if mnum == 2:
+            match_code = mcode + "3"
+            return EVENT_CODE, match_code
+
+
 def initialize_upload(youtube, options):
-  tags = None
-  if options.keywords:
-    tags = options.keywords.split(",")
+    tags = None
+    match_data = get_match_results(get_match_code(options.mcode, options.mnum))
+    blue1, blue2, blue3, blue_score, red1, red2, red3, red_score = parse_data(
+        match_data)
 
-  body=dict(
-    snippet=dict(
-      title=options.title % options.mnum,
-      description=options.description,
-      tags=tags,
-      categoryId=options.category
-    ),
-    status=dict(
-      privacyStatus=options.privacyStatus
+    if options.keywords:
+        tags = options.keywords.split(",")
+
+    body = dict(
+        snippet=dict(
+            title=options.title % options.mnum,
+            description=options.description % blue1, blue2, blue3, blue_score, red1, red2, red3, red_score,
+            tags=tags,
+            categoryId=options.category
+        ),
+        status=dict(
+            privacyStatus=options.privacyStatus
+        )
     )
-  )
 
-  # Call the API's videos.insert method to create and upload the video.
-  insert_request = youtube.videos().insert(
-    part=",".join(body.keys()),
-    body=body,
-    # The chunksize parameter specifies the size of each chunk of data, in
-    # bytes, that will be uploaded at a time. Set a higher value for
-    # reliable connections as fewer chunks lead to faster uploads. Set a lower
-    # value for better recovery on less reliable connections.
-    #
-    # Setting "chunksize" equal to -1 in the code below means that the entire
-    # file will be uploaded in a single HTTP request. (If the upload fails,
-    # it will still be retried where it left off.) This is usually a best
-    # practice, but if you're using Python older than 2.6 or if you're
-    # running on App Engine, you should set the chunksize to something like
-    # 1024 * 1024 (1 megabyte).
-    media_body=MediaFileUpload(options.file % options.mnum, chunksize=-1, resumable=True)
-  )
+    # Call the API's videos.insert method to create and upload the video.
+    insert_request = youtube.videos().insert(
+        part=",".join(body.keys()),
+        body=body,
+        # The chunksize parameter specifies the size of each chunk of data, in
+        # bytes, that will be uploaded at a time. Set a higher value for
+        # reliable connections as fewer chunks lead to faster uploads. Set a lower
+        # value for better recovery on less reliable connections.
+        #
+        # Setting "chunksize" equal to -1 in the code below means that the entire
+        # file will be uploaded in a single HTTP request. (If the upload fails,
+        # it will still be retried where it left off.) This is usually a best
+        # practice, but if you're using Python older than 2.6 or if you're
+        # running on App Engine, you should set the chunksize to something like
+        # 1024 * 1024 (1 megabyte).
+        media_body=MediaFileUpload(
+            options.file % options.mnum, chunksize=-1, resumable=True)
+    )
 
-  resumable_upload(insert_request, options.mnum, options.mcode, youtube)
+    resumable_upload(insert_request, options.mnum, options.mcode, youtube)
 
 # This method implements an exponential backoff strategy to resume a
 # failed upload.
+
+
 def resumable_upload(insert_request, mnum, mcode, youtube):
-  response = None
-  error = None
-  retry = 0
-  while response is None:
-    try:
-      print "Uploading file..."
-      status, response = insert_request.next_chunk()
-      if 'id' in response:
-        print "Video id '%s' was successfully uploaded." % response['id']
-        print "Video link is https://www.youtube.com/watch?v=%s" % response['id']
-        update_thumbnail(youtube, response['id'], "thumbnail.png")
-        print "Video thumbnail added"
-        add_video_to_playlist(youtube,response['id'],DEFAULT_PLAYLIST_ID)
-        #os.system("python addtoplaylist.py --vID " + response['id']) #Use as backup to the previous line
-        pyperclip.copy('https://www.youtube.com/watch?v=%s' % response['id'])
-        spam = pyperclip.paste()
-        print "YouTube link copied to clipboard for TheBlueAlliance"
+    response = None
+    error = None
+    retry = 0
+    while response is None:
+        try:
+            print "Uploading file..."
+            status, response = insert_request.next_chunk()
+            if 'id' in response:
+                print "Video id '%s' was successfully uploaded." % response['id']
+                print "Video link is https://www.youtube.com/watch?v=%s" % response['id']
+                update_thumbnail(youtube, response['id'], "thumbnail.png")
+                print "Video thumbnail added"
+                add_video_to_playlist(
+                    youtube, response['id'], DEFAULT_PLAYLIST_ID)
+                # os.system("python addtoplaylist.py --vID " + response['id'])
+                # #Use as backup to the previous line
+                pyperclip.copy(
+                    'https://www.youtube.com/watch?v=%s' % response['id'])
+                spam = pyperclip.paste()
+                print "YouTube link copied to clipboard for TheBlueAlliance"
 
-      else:
-        exit("The upload failed with an unexpected response: %s" % response)
-    except HttpError, e:
-      if e.resp.status in RETRIABLE_STATUS_CODES:
-        error = "A retriable HTTP error %d occurred:\n%s" % (e.resp.status,
-                                                             e.content)
-      else:
-        raise
-    except RETRIABLE_EXCEPTIONS, e:
-      error = "A retriable error occurred: %s" % e
+            else:
+                exit("The upload failed with an unexpected response: %s" %
+                     response)
+        except HttpError, e:
+            if e.resp.status in RETRIABLE_STATUS_CODES:
+                error = "A retriable HTTP error %d occurred:\n%s" % (e.resp.status,
+                                                                     e.content)
+            else:
+                raise
+        except RETRIABLE_EXCEPTIONS, e:
+            error = "A retriable error occurred: %s" % e
 
-    if error is not None:
-      print error
-      retry += 1
-      if retry > MAX_RETRIES:
-        exit("No longer attempting to retry.")
+        if error is not None:
+            print error
+            retry += 1
+            if retry > MAX_RETRIES:
+                exit("No longer attempting to retry.")
 
-      max_sleep = 2 ** retry
-      sleep_seconds = random.random() * max_sleep
-      print "Sleeping %f seconds and then retrying..." % sleep_seconds
-      time.sleep(sleep_seconds)
+            max_sleep = 2 ** retry
+            sleep_seconds = random.random() * max_sleep
+            print "Sleeping %f seconds and then retrying..." % sleep_seconds
+            time.sleep(sleep_seconds)
 
 if __name__ == '__main__':
-    argparser.add_argument("--mnum", help="Match Number to add", required=True)
-    argparser.add_argument("--mcode", help="Match code (qm,qf,sf,f) starting at 0 ->3", default=0)
-    argparser.add_argument("--file", help="Video file to upload", default=DEFAULT_FILE)
-    argparser.add_argument("--title", help="Video title", default=DEFAULT_TITLE)
-    argparser.add_argument("--description", help="Video description", default=DEFAULT_DESCRIPTION)
-    argparser.add_argument("--category", default=DEFAULT_VIDEO_CATEGORY, help="Numeric video category. " +"See https://developers.google.com/youtube/v3/docs/videoCategories/list")
-    argparser.add_argument("--keywords", help="Video keywords, comma separated", default=DEFAULT_TAGS)
-    argparser.add_argument("--privacyStatus", choices=VALID_PRIVACY_STATUSES, default=VALID_PRIVACY_STATUSES[2], help="Video privacy status.")
+    argparser.add_argument("--mnum", help="""Match Number to add, if in elims, 
+      keep incrementing by one unless for tiebreaker, in which case add 8 to the tiebreaker number""", required=True)
+    argparser.add_argument(
+        "--mcode", help="Match code (qm,qf,sf,f) starting at 0 ->3", default=0)
+    argparser.add_argument(
+        "--file", help="Video file to upload", default=DEFAULT_FILE)
+    argparser.add_argument(
+        "--title", help="Video title", default=DEFAULT_TITLE)
+    argparser.add_argument(
+        "--description", help="Video description", default=DEFAULT_DESCRIPTION)
+    argparser.add_argument("--category", default=DEFAULT_VIDEO_CATEGORY, help="Numeric video category. " +
+                           "See https://developers.google.com/youtube/v3/docs/videoCategories/list")
+    argparser.add_argument(
+        "--keywords", help="Video keywords, comma separated", default=DEFAULT_TAGS)
+    argparser.add_argument("--privacyStatus", choices=VALID_PRIVACY_STATUSES,
+                           default=VALID_PRIVACY_STATUSES[2], help="Video privacy status.")
     args = argparser.parse_args()
 
     youtube = get_authenticated_service(args)
-    event_code = get_events_and_codes(2015, 'Silicon Valley')[1]
-    event = Event('github_user', 'test', '1.0', event_code)
 
-    event.get_event_info()
     try:
-     initialize_upload(youtube, args)
+        initialize_upload(youtube, args)
     except HttpError, e:
         print "An HTTP error %d occurred:\n%s" % (e.resp.status, e.content)
