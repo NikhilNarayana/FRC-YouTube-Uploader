5d7b5ae4451f73fb651570fbf431c54ca4b3aa40
Nikhil Narayana <nikhil.narayana@live.com>
2016-10-19 21:13:05 -0400

    Attempt async non-blocking webserver

--- start.py
+++ start.py
@@ -6,11 +6,13 @@ import youtubeup as yup
 import argparse
 import csv
 from datetime import datetime
+import threading
+from time import sleep
 
 render = web.template.render('webpage/')
 
-urls = ('/', 'index')
-app = web.application(urls, globals())
+# urls = ('/', 'index')
+# app = web.application(urls, globals())
 
 dataform = form.Form(
 	form.Dropdown("where",
@@ -55,7 +57,12 @@ dataform = form.Form(
 		lambda i: i.end == "Only for batch uploads" or int(i.end) > int(i.mnum))]
 	)
 
-class index:
+class index(threading.Thread):
+	def run(self):
+		urls = ('/', 'index')
+		app = web.application(urls, globals())
+		app.run()
+
 	def GET(self):
 		form = dataform()
 		with open('form_values.csv', 'rb') as csvfile:
@@ -82,7 +89,7 @@ class index:
 							14: form.tba,
 							15: form.end,
 						}
-						if i == 13 or i == 14:
+						if i == 13 or i == 14 or i == 0:
 							if value == "True": switcher[i].set_value(True)
 							if value == "False": switcher[i].set_value(False)
 						else : switcher[i].set_value(value)
@@ -119,18 +126,25 @@ class index:
 			args.tiebreak, row[13] = formdata.has_key('tiebreak'), str(formdata.has_key('tiebreak'))
 			args.tba, row[14] = formdata.has_key('tba'), str(formdata.has_key('tba'))
 			args.end = row[15] = form.d.end
-			yup.init(args)
+			thr = threading.Thread(target=yup.init, args=(args,))
+			thr.daemon = True
+			thr.start()
 			if form.d.end == "Only for batch uploads":
 				form.mnum.set_value(str(int(form.d.mnum) + 1))
 			else:
 				form.mnum.set_value(str(int(form.d.end) + 1))
 				form.end.set_value("Only for batch uploads")
-			row[6] = int(form.d.mnum)
-			row[10] = form.d.end
+			row[11] = int(form.d.mnum)
+			row[15] = form.d.end
 			writer = csv.writer(open('form_values.csv', 'w'))
 			writer.writerow(row)
+			thr.join()
 			return render.forms(form)
 
 if __name__=="__main__":
 	web.internalerror = web.debugerror
-	app.run()
+	t = index()
+	t.daemon = True
+	t.start()
+	while True:
+		sleep(100)
