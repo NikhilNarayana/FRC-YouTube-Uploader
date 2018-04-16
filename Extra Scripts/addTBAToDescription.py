72a07f9c3f4352dc7cac7d3e79350e6ff51d68ab
Nikhil Narayana <nikhil.narayana@live.com>
2018-04-16 17:40:48 -0400

    Fix addTBAToDescription

--- Extra Scripts/addTBAToDescription.py
+++ Extra Scripts/addTBAToDescription.py
@@ -17,19 +17,15 @@ credits = """
 Updated with FRC-Youtube-Uploader (https://github.com/NikhilNarayana/FRC-YouTube-Uploader)"""
 
 
-def run(youtube, pID, ecode, mID, mnum, end):
-    vids = get_video_ids(youtube, pID)
+def run(youtube, ID, ecode, mID, mnum, end):
     if end != 0:
-        while int(mnum) <= int(end):
-            mnum = int(mnum) + 1
-            vID = video_id(vURL)
-            update_description(youtube, vID, ecode, mID, mnum)
-            vURL = "https://www.youtube.com/watch?v={}".format(
-                get_next_video_id(youtube, vID, pID))
-        print("Updated all video descriptions")
-        return vURL
+        for vid in get_video_ids(youtube, ID):
+            if int(mnum) <= int(end):
+                mnum = int(mnum) + 1
+                update_description(youtube, vid, ecode, mID, mnum)
     else:
-        return vURL
+        update_description(youtube, ID, ecode, mID, mnum)
+    print("Updated all video descriptions")
 
 
 # http://stackoverflow.com/questions/4356538/how-can-i-extract-video-id-from-youtubes-link-in-python
@@ -70,7 +66,8 @@ def get_video_ids(youtube, pID):
             playlistitems_list.pop('nextPageToken', None)
         else:
             nextPageToken = nextPageList['nextPageToken']
-    return playlistitems_list  # if you get here the playlistID must not work
+    for item in playlistitems_list["items"]:
+        yield item["snippet"]["resourceId"]["videoId"]
 
 
 def update_description(youtube, vID, ecode, mID, mnum):
@@ -78,8 +75,8 @@ def update_description(youtube, vID, ecode, mID, mnum):
         part="snippet",
         id=vID).execute()
     olddesc = snippet['items'][0]['snippet']['description']
-    mcode = yup.get_match_code(mID, int(mnum))
-    blue_data, red_data = TBA.get_match_results(ecode, mcode)
+    mcode = yup.get_match_code(mID, int(mnum), "")
+    blue_data, red_data = yup.get_match_results(ecode, mcode)
     newdesc = data.format(red_data[1], red_data[2], red_data[3], red_data[0], blue_data[1], blue_data[2], blue_data[3], blue_data[0]) + olddesc + credits
     snippet['items'][0]['snippet']['description'] = newdesc
     youtube.videos().update(
@@ -116,13 +113,15 @@ dataform = form.Form(
 
 
 if __name__ == "__main__":
-    print("Not currently working")
-    sys.exit(0)
-    PID = input("Playlist Link: ")
     ECODE = input("Event Code: ")
     MCODE = input("Match Type (qm, qf, sf, or f1m)")
     MNUM = int(input("First Match Number: "))
     END = int(input("Last Match Number (0 if only updating one video): "))
-    PID = PID[f:f+34]
+    ID = None
+    if not END:
+        ID = video_id(input("Video link: "))
+    else:
+        ID = input("Playlist Link: ")
+        ID = PID[f:f+34]
 
-    run(youtube, )
+    run(youtube, ID, ECODE, MCODE, MNUM, END)
